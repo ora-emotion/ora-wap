e07338201f14f31e97a3f16ecb4f40ef9055d8ff
smpower <bzsjxhywrf@outlook.com>
2017-10-13 15:11:18 +0800

    关于我们页面添加模块标题

--- js/about/spa.about.js
+++ js/about/spa.about.js
@@ -1,6 +1,6 @@
 /*
  * spa.about.js
- * about us module for SPA
+ * About us module for SPA
  * 关于我们
 */
 
@@ -15,7 +15,14 @@
 spa.about = (function () {
   var
     configMap = {
-      main_html : String() + '关于我们页面'
+      main_html : String()
+        + '<div class="about-ora"></div>'
+        + '<div class="about-history"></div>'
+        + '<div class="about-environment"></div>'
+        + '<div class="about-dynamic"></div>'
+        + '<div class="about-step"></div>'
+        + '<div class="about-guarantee"></div>'
+        + '<div class="about-associated"></div>'
     },
     stateMap = { $container : null, $about: null },
     jqueryMap = {},
@@ -27,8 +34,15 @@ spa.about = (function () {
       $about     = stateMap.$about;
 
     jqueryMap = {
-      $container : $container,
-      $about     : $about
+      $container   : $container,
+      $about       : $about,
+      $ora         : $about.find('.about-ora'),
+      $history     : $about.find('.about-history'),
+      $environment : $about.find('.about-environment'),
+      $dynamic     : $about.find('.about-dynamic'),
+      $step        : $about.find('.about-step'),
+      $guarantee   : $about.find('.about-guarantee'),
+      $associated  : $about.find('.about-associated')
     };
   };
 
@@ -39,6 +53,13 @@ spa.about = (function () {
     preface_img.attr('src', 'images/about/preface.png');
 
     // 加载模块
+    spa.about.ora.initModule(jqueryMap.$ora);
+    spa.about.history.initModule(jqueryMap.$history);
+    spa.about.environment.initModule(jqueryMap.$environment);
+    spa.about.dynamic.initModule(jqueryMap.$dynamic);
+    spa.about.step.initModule(jqueryMap.$step);
+    spa.about.guarantee.initModule(jqueryMap.$guarantee);
+    spa.about.associated.initModule(jqueryMap.$associated);
   };
 
   initModule = function ($container, $about) {
